13b9f471c432ae8cb9c6164b5b00f9da6094b3d7
KnechtRootrechtCH <github@atheos.cc>
2018-07-23 14:27:14 +0200

    #40: export improvements
    * added header
    * introduces single file component

---
+++ src/components/Export.vue
@@ -0,0 +1,163 @@
+<template>
+    <span id="export" class="export" @click="exportList">
+      <span><font-awesome-icon :icon="icon('download')"/></span>
+      <span class="label">{{ $t('export') }}</span>
+    </span>
+</template>
+
+<script>
+import MetadataMixin from '../mixins/metadata'
+import UtilsMixin from '../mixins/utils'
+import IconsMixin from '../mixins/icons'
+
+export default {
+  name: 'MediaList',
+  mixins: [UtilsMixin, MetadataMixin, IconsMixin],
+  props: ['items', 'sort', 'filter'],
+  data () {
+    return {
+    }
+  },
+  components: {
+  },
+  computed: {
+  },
+  methods: {
+    getHeader () {
+      let header = this.$t('exportHeader').toUpperCase()
+      // header += '\n' + title.replace(/./g, '=')
+      header += '\n' + this.$t('statusFilter') + ': ' + this.getStatusFilter()
+      header += '\n' + this.$t('priorityFilter') + ': ' + this.getPriorityFilter()
+      header += '\n' + this.$t('sort') + ': ' + this.getSort()
+      header += '\n\n'
+      return header
+    },
+    getStatusFilter () {
+      let s = []
+      if (this.filter.downloaded && this.filter.queued && this.filter.todo && this.filter.hardToFind && this.filter.notYetAvailable) {
+        return this.$t('all')
+      }
+      if (this.filter.downloaded) {
+        s.push(this.$t('downloaded'))
+      }
+      if (this.filter.queued) {
+        s.push(this.$t('queued'))
+      }
+      if (this.filter.todo) {
+        s.push(this.$t('todo'))
+      }
+      if (this.filter.hardToFind) {
+        s.push(this.$t('hardToFind'))
+      }
+      if (this.filter.notYetAvailable) {
+        s.push(this.$t('notYetAvailable'))
+      }
+      return s.join(', ')
+    },
+    getPriorityFilter () {
+      if (this.filter.priority1 && this.filter.priority2 && this.filter.priority3 && this.filter.priority4 && this.filter.priority5) {
+        return this.$t('all')
+      }
+      let priorities = []
+      if (this.filter.priority1 && this.settings.priority.min >= 1) {
+        priorities.push('*'.repeat(this.settings.priority.min - 0))
+      }
+      if (this.filter.priority2 && this.settings.priority.min >= 2) {
+        priorities.push('*'.repeat(this.settings.priority.min - 1))
+      }
+      if (this.filter.priority3 && this.settings.priority.min >= 3) {
+        priorities.push('*'.repeat(this.settings.priority.min - 2))
+      }
+      if (this.filter.priority4 && this.settings.priority.min >= 4) {
+        priorities.push('*'.repeat(this.settings.priority.min - 3))
+      }
+      if (this.filter.priority5 && this.settings.priority.min >= 5) {
+        priorities.push('*'.repeat(this.settings.priority.min - 4))
+      }
+      return priorities.join(', ')
+    },
+    getSort () {
+      return this.$t(this.sort)
+    },
+    getExportData () {
+      let content = ''
+      this.items.forEach(i => {
+        content += this.getTitle(i) + ' (' + this.getReleaseDateFormated(i, 'YYYY') + ')\n'
+      })
+
+      return content
+    },
+    exportList () {
+      let date = this.$moment().format('YYYYMMDD_HHmm')
+      let fileName = 'export_' + date + '.txt'
+      let data = this.getHeader()
+      data += this.getExportData()
+
+      let blob = new Blob([data], {type: 'text/plain;charset=utf-8'})
+      if (window.navigator.msSaveOrOpenBlob) {
+        window.navigator.msSaveBlob(blob, fileName)
+      } else {
+        let elem = window.document.createElement('a')
+        elem.href = window.URL.createObjectURL(blob)
+        elem.download = fileName
+        document.body.appendChild(elem)
+        elem.click()
+        document.body.removeChild(elem)
+      }
+    }
+  },
+  i18n: {
+    messages: {
+      de: {
+        export: 'Liste exportieren',
+        exportHeader: 'Download Liste',
+        statusFilter: 'Status',
+        priorityFilter: 'Priorität',
+        sort: 'Sortierung',
+        all: 'Alle',
+        notYetAvailable: 'Noch nicht erhältlich',
+        hardToFind: 'Noch nicht gefunden',
+        queued: 'Wird heruntergeladen',
+        todo: 'Todo',
+        downloaded: 'Heruntergeladen',
+        priority: 'Priorität',
+        rating: 'Bewertung',
+        title: 'Titel',
+        popularity: 'Beliebtheit',
+        release: 'Veröffentlichung'
+      },
+      en: {
+        export: 'Export list',
+        exportHeader: 'Download list',
+        statusFilter: 'Status',
+        priorityFilter: 'Priority',
+        sort: 'Sorting',
+        all: 'Any',
+        notYetAvailable: 'Not available yet',
+        hardToFind: 'Not found yet',
+        queued: 'Queued',
+        todo: 'Todo',
+        downloaded: 'Downloaded',
+        priority: 'Priority',
+        rating: 'Rating',
+        title: 'Title',
+        popularity: 'Popularity',
+        release: 'Release'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.export {
+  cursor: pointer;
+  color: skyblue;
+}
+.export:hover {
+  color: silver;
+}
+.label {
+  margin-left: 5px;
+}
+</style>
--- src/components/MediaList.vue
+++ src/components/MediaList.vue
@@ -1,41 +1,44 @@
 <template>
   <div class="media-list">
-      <div v-for="item in pagedItems" :key="item.key" class="list-item">
-        <span>
-          <font-awesome-icon
-            :icon="icon(itemStatusIconName(item))"
-            v-bind:class="{ 'green': itemIsDownloaded(item), 'skyblue' : itemIsQueued(item), 'orange': itemIsUnreleased(item), 'yellow': itemIsNotYetAvailable(item) || itemIsHardToFind(item) }"
-          />
-          <router-link v-bind:to="infoUrl(item)"><a class="label">{{ getTitle(item) }}</a></router-link>
-          <span>({{ getReleaseDateFormated(item, 'YYYY') }})</span>
-          <!--
-          <span class="item-status d-none d-lg-inline">
-            <span>&ndash;</span>
-            <span v-if="item.downloaded">{{ $t('downloaded') }}</span>
-            <span v-if="!item.downloaded && itemIsUnreleased(item)">{{ $t('unreleased') }}</span>
-            <span v-if="!item.downloaded && !itemIsUnreleased(item) && item.downloadStatus">{{ $t(item.downloadStatus) }}</span>
-            <span v-if="!item.downloaded && !itemIsUnreleased(item) && !item.downloadStatus">{{ $t('todo') }}</span>
-          </span>
-          -->
+    <div class="export">
+      <export v-bind:items="sortedItems"
+        v-bind:sort="sort"
+        v-bind:filter="filter"/>
+    </div>
+    <div v-for="item in pagedItems" :key="item.key" class="list-item">
+      <span>
+        <font-awesome-icon
+          :icon="icon(itemStatusIconName(item))"
+          v-bind:class="{ 'green': itemIsDownloaded(item), 'skyblue' : itemIsQueued(item), 'orange': itemIsUnreleased(item), 'yellow': itemIsNotYetAvailable(item) || itemIsHardToFind(item) }"
+        />
+        <router-link v-bind:to="infoUrl(item)"><a class="label">{{ getTitle(item) }}</a></router-link>
+        <span>({{ getReleaseDateFormated(item, 'YYYY') }})</span>
+        <!--
+        <span class="item-status d-none d-lg-inline">
+          <span>&ndash;</span>
+          <span v-if="item.downloaded">{{ $t('downloaded') }}</span>
+          <span v-if="!item.downloaded && itemIsUnreleased(item)">{{ $t('unreleased') }}</span>
+          <span v-if="!item.downloaded && !itemIsUnreleased(item) && item.downloadStatus">{{ $t(item.downloadStatus) }}</span>
+          <span v-if="!item.downloaded && !itemIsUnreleased(item) && !item.downloadStatus">{{ $t('todo') }}</span>
         </span>
-        <span class="float-right d-none d-sm-inline">
-          <font-awesome-icon
-            v-for="p in priorities"
-            :key="p"
-            :icon="icon('star')"
-            class="priority-icon"
-            v-bind:class="[{ 'highlighted' :item.priority <= p }, icon]"
-            @click.stop="setItemPriority(item, p)" @mouseover="hoverPriority = p" @mouseout="hoverPriority = settings.priority.min + 1"/>
-        </span>
-      </div>
-      <div id="export" class="export" @click="exportList">
-        <span><font-awesome-icon :icon="icon('download')"/></span>
-        <span class="label">{{ $t('export') }}</span>
-      </div>
+        -->
+      </span>
+      <span class="float-right d-none d-sm-inline">
+        <font-awesome-icon
+          v-for="p in priorities"
+          :key="p"
+          :icon="icon('star')"
+          class="priority-icon"
+          v-bind:class="[{ 'highlighted' :item.priority <= p }, icon]"
+          @click.stop="setItemPriority(item, p)" @mouseover="hoverPriority = p" @mouseout="hoverPriority = settings.priority.min + 1"/>
+      </span>
+    </div>
   </div>
 </template>
 
 <script>
+import Export from './Export'
+
 import MetadataMixin from '../mixins/metadata'
 import UtilsMixin from '../mixins/utils'
 import IconsMixin from '../mixins/icons'
@@ -49,6 +52,7 @@ export default {
     }
   },
   components: {
+    'export': Export
   },
   computed: {
     filteredItems () {
@@ -79,34 +83,6 @@ export default {
     infoUrl (item) {
       let infoUrl = '/list/' + item.media_type + '/' + item.id
       return infoUrl
-    },
-    getItemListExport () {
-      let content = ''
-      this.sortedItems.forEach(i => {
-        content += this.getTitle(i) + ' (' + this.getReleaseDateFormated(i, 'YYYY') + ')\n'
-      })
-      content += '\n+--------------------------------+'
-      content += '\nfilter: ' + JSON.stringify(this.filter)
-      content += '\nsort: ' + this.sort
-      content += '\n+--------------------------------+'
-      return content
-    },
-    exportList () {
-      let data = this.getItemListExport()
-      let date = this.$moment().format('YYYYMMDD_HHmm')
-      let fileName = 'export_' + date + '.txt'
-
-      let blob = new Blob([data], {type: 'text/plain;charset=utf-8'})
-      if (window.navigator.msSaveOrOpenBlob) {
-        window.navigator.msSaveBlob(blob, fileName)
-      } else {
-        let elem = window.document.createElement('a')
-        elem.href = window.URL.createObjectURL(blob)
-        elem.download = fileName
-        document.body.appendChild(elem)
-        elem.click()
-        document.body.removeChild(elem)
-      }
     }
   },
   i18n: {
@@ -168,11 +144,6 @@ export default {
   margin-left: 5px;
 }
 .export {
-  margin-top: 20px;
-  cursor: pointer;
-  color: skyblue;
-}
-.export:hover {
-  color: silver;
+  margin-bottom: 10px;
 }
 </style>
